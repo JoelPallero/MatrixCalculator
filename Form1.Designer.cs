9a80af90ba41c4f4911fa3da0b9b2b431f37bb5d
joel pallero <estebanjoelpallero@hotmail.com>
2021-10-02 11:01:02 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace MatrixCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgvDatos = new System.Windows.Forms.DataGridView();
+            this.dtgvResultado = new System.Windows.Forms.DataGridView();
+            this.txtFil = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnResul = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCargarMatriz = new System.Windows.Forms.Button();
+            this.lblCargarMatriz = new System.Windows.Forms.Label();
+            this.txtCol = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPm = new System.Windows.Forms.Button();
+            this.btnPe = new System.Windows.Forms.Button();
+            this.btnTras = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvDatos
+            // 
+            this.dtgvDatos.AllowUserToAddRows = false;
+            this.dtgvDatos.AllowUserToDeleteRows = false;
+            this.dtgvDatos.AllowUserToOrderColumns = true;
+            this.dtgvDatos.AllowUserToResizeColumns = false;
+            this.dtgvDatos.AllowUserToResizeRows = false;
+            this.dtgvDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgvDatos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dtgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvDatos.ColumnHeadersVisible = false;
+            this.dtgvDatos.Location = new System.Drawing.Point(12, 12);
+            this.dtgvDatos.Name = "dtgvDatos";
+            this.dtgvDatos.RowHeadersVisible = false;
+            this.dtgvDatos.RowHeadersWidth = 51;
+            this.dtgvDatos.RowTemplate.Height = 29;
+            this.dtgvDatos.Size = new System.Drawing.Size(385, 243);
+            this.dtgvDatos.TabIndex = 0;
+            // 
+            // dtgvResultado
+            // 
+            this.dtgvResultado.AllowUserToAddRows = false;
+            this.dtgvResultado.AllowUserToDeleteRows = false;
+            this.dtgvResultado.AllowUserToResizeColumns = false;
+            this.dtgvResultado.AllowUserToResizeRows = false;
+            this.dtgvResultado.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgvResultado.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dtgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvResultado.ColumnHeadersVisible = false;
+            this.dtgvResultado.Location = new System.Drawing.Point(12, 288);
+            this.dtgvResultado.Name = "dtgvResultado";
+            this.dtgvResultado.RowHeadersVisible = false;
+            this.dtgvResultado.RowHeadersWidth = 51;
+            this.dtgvResultado.RowTemplate.Height = 29;
+            this.dtgvResultado.Size = new System.Drawing.Size(385, 304);
+            this.dtgvResultado.TabIndex = 1;
+            // 
+            // txtFil
+            // 
+            this.txtFil.Location = new System.Drawing.Point(414, 46);
+            this.txtFil.Name = "txtFil";
+            this.txtFil.Size = new System.Drawing.Size(57, 27);
+            this.txtFil.TabIndex = 3;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Enabled = false;
+            this.btnSumar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSumar.Location = new System.Drawing.Point(414, 89);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(148, 47);
+            this.btnSumar.TabIndex = 4;
+            this.btnSumar.Text = "+";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Enabled = false;
+            this.btnRestar.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRestar.Location = new System.Drawing.Point(599, 89);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(148, 47);
+            this.btnRestar.TabIndex = 5;
+            this.btnRestar.Text = "-";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            this.btnRestar.Click += new System.EventHandler(this.btnRestar_Click);
+            // 
+            // btnResul
+            // 
+            this.btnResul.Location = new System.Drawing.Point(414, 226);
+            this.btnResul.Name = "btnResul";
+            this.btnResul.Size = new System.Drawing.Size(148, 29);
+            this.btnResul.TabIndex = 6;
+            this.btnResul.Text = "Resultado";
+            this.btnResul.UseVisualStyleBackColor = true;
+            this.btnResul.Click += new System.EventHandler(this.btnResul_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(414, 556);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(148, 36);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCargarMatriz
+            // 
+            this.btnCargarMatriz.Location = new System.Drawing.Point(599, 46);
+            this.btnCargarMatriz.Name = "btnCargarMatriz";
+            this.btnCargarMatriz.Size = new System.Drawing.Size(148, 29);
+            this.btnCargarMatriz.TabIndex = 2;
+            this.btnCargarMatriz.Text = "Cargar Matriz";
+            this.btnCargarMatriz.UseVisualStyleBackColor = true;
+            this.btnCargarMatriz.Click += new System.EventHandler(this.btnCargarMatriz_Click);
+            // 
+            // lblCargarMatriz
+            // 
+            this.lblCargarMatriz.AutoSize = true;
+            this.lblCargarMatriz.Location = new System.Drawing.Point(414, 15);
+            this.lblCargarMatriz.Name = "lblCargarMatriz";
+            this.lblCargarMatriz.Size = new System.Drawing.Size(186, 20);
+            this.lblCargarMatriz.TabIndex = 8;
+            this.lblCargarMatriz.Text = "Elegir Tamaño de la Matriz";
+            // 
+            // txtCol
+            // 
+            this.txtCol.Location = new System.Drawing.Point(505, 46);
+            this.txtCol.Name = "txtCol";
+            this.txtCol.Size = new System.Drawing.Size(57, 27);
+            this.txtCol.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(480, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "x";
+            // 
+            // btnPm
+            // 
+            this.btnPm.Enabled = false;
+            this.btnPm.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPm.Location = new System.Drawing.Point(599, 142);
+            this.btnPm.Name = "btnPm";
+            this.btnPm.Size = new System.Drawing.Size(148, 47);
+            this.btnPm.TabIndex = 12;
+            this.btnPm.Text = "* (PM)";
+            this.btnPm.UseVisualStyleBackColor = true;
+            this.btnPm.Click += new System.EventHandler(this.btnPm_Click);
+            // 
+            // btnPe
+            // 
+            this.btnPe.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnPe.Location = new System.Drawing.Point(414, 142);
+            this.btnPe.Name = "btnPe";
+            this.btnPe.Size = new System.Drawing.Size(148, 47);
+            this.btnPe.TabIndex = 11;
+            this.btnPe.Text = "* (EM)";
+            this.btnPe.UseVisualStyleBackColor = true;
+            this.btnPe.Click += new System.EventHandler(this.btnPe_Click);
+            // 
+            // btnTras
+            // 
+            this.btnTras.Enabled = false;
+            this.btnTras.Location = new System.Drawing.Point(599, 226);
+            this.btnTras.Name = "btnTras";
+            this.btnTras.Size = new System.Drawing.Size(148, 29);
+            this.btnTras.TabIndex = 13;
+            this.btnTras.Text = "Traspuesta";
+            this.btnTras.UseVisualStyleBackColor = true;
+            this.btnTras.Click += new System.EventHandler(this.btnTras_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 598);
+            this.Controls.Add(this.btnTras);
+            this.Controls.Add(this.btnPm);
+            this.Controls.Add(this.btnPe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCol);
+            this.Controls.Add(this.lblCargarMatriz);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnResul);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtFil);
+            this.Controls.Add(this.btnCargarMatriz);
+            this.Controls.Add(this.dtgvResultado);
+            this.Controls.Add(this.dtgvDatos);
+            this.Name = "Form1";
+            this.Text = "Matrices";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvDatos;
+        private System.Windows.Forms.DataGridView dtgvResultado;
+        private System.Windows.Forms.TextBox txtFil;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnResul;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCargarMatriz;
+        private System.Windows.Forms.Label lblCargarMatriz;
+        private System.Windows.Forms.TextBox txtCol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPm;
+        private System.Windows.Forms.Button btnPe;
+        private System.Windows.Forms.Button btnTras;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrixCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Variables
+        int nFil;
+        int nCol;
+        int nFilB;
+        int nColB;
+        string operacion;
+        bool txtvacio;
+        decimal[,] matriz1;
+        decimal[,] matriz2;
+        int k;
+
+        #endregion
+
+        private void btnCargarMatriz_Click(object sender, EventArgs e)
+        {
+            ControlText();
+            if (txtvacio)
+            {
+                MessageBox.Show("No puede haber casilleros vacíos", "Completar datos", MessageBoxButtons.OK);
+            }
+            else
+            {
+                if (nFil < 1)
+                {
+                    nFil = Convert.ToInt32(txtFil.Text);
+                    nCol = Convert.ToInt32(txtCol.Text);
+                    DimensionMatrizA();
+                }
+                else
+                {
+                    nFilB = Convert.ToInt32(txtFil.Text);
+                    nColB = Convert.ToInt32(txtCol.Text);
+                }
+
+                switch (operacion)
+                {
+                    case "prodem":
+                        DimensionMatrizR();
+                        break;
+                    case "producto":
+                        if (nCol != nFilB)
+                        {
+                            MessageBox.Show("Las columnas de la primera matriz no coincide con las filas de esta nueva matriz. Tiene que tener " + nCol + " filas.", "No se puede realizar la opreación", MessageBoxButtons.OK);
+                        }
+                        else
+                        {
+                            DimensionMatrizB();
+                            DimensionMatrizR();
+                        }
+                        break;
+                }
+                btnSumar.Enabled = true;
+                btnRestar.Enabled = true;
+                btnPm.Enabled = true;
+                btnTras.Enabled = true;
+            }
+            ClearControl();
+        }
+
+        private void DimensionMatrizA()
+        {
+            for (int i = 0; i < nCol; i++)
+            {
+                DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+                column1.HeaderText = "N";
+                dtgvDatos.Columns.Add(column1);
+            }
+            dtgvDatos.Rows.Add(nFil);
+        }
+
+        private void DimensionMatrizB()
+        {
+            for (int i = 0; i < nColB; i++)
+            {
+                DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+                column1.HeaderText = "N";
+                dtgvDatos.Columns.Add(column1);
+            }
+            dtgvDatos.Rows.Add(nFilB);
+        }
+
+        private void DimensionMatrizR()
+        {
+            if (operacion == "producto")
+            {
+                for (int i = 0; i < nColB; i++)
+                {
+                    DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+                    column1.HeaderText = "N";
+                    dtgvResultado.Columns.Add(column1);
+                }
+                dtgvResultado.Rows.Add(nFil);
+            }
+            else
+            {
+                for (int i = 0; i < nCol; i++)
+                {
+                    DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+                    column1.HeaderText = "N";
+                    dtgvResultado.Columns.Add(column1);
+                }
+                dtgvResultado.Rows.Add(nFil);
+            }            
+        }
+
+
+        private void ControlText()
+        {
+            foreach (var txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt is TextBox)
+                {
+                    if (string.IsNullOrEmpty(txt.Text))
+                    {
+                        txtvacio = true;
+                    }
+                    else
+                    {
+                        txtvacio = false;
+                    }
+                }
+            }
+        }
+
+        private void ControlDtgv()
+        {
+            foreach (var txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt is TextBox)
+                {
+                    if (string.IsNullOrEmpty(txt.Text))
+                    {
+                        txtvacio = true;
+                    }
+                    else
+                    {
+                        txtvacio = false;
+                    }
+                }
+            }
+        }
+
+        private void CleanDtgd()
+        {
+            foreach (var dgv in this.Controls.OfType<DataGridView>())
+            {
+                dgv.Rows.Clear();
+                dgv.Columns.Clear();
+            }
+        }
+
+        private void ClearControl()
+        {
+            foreach (var txt in this.Controls.OfType<TextBox>())
+            {
+                if (txt is TextBox)
+                {
+                    txt.Clear();
+                }
+            }
+        }
+
+        private void AsignarMatriz()
+        {
+            matriz1 = new decimal[nFil, nCol];
+            for (int i = 0; i < nFil; i++)
+            {
+                for (int j = 0; j < nCol; j++)
+                {
+                    matriz1[i, j] = Convert.ToDecimal(dtgvDatos.Rows[i].Cells[j].Value);
+                }
+            }
+        }
+        private void btnResul_Click(object sender, EventArgs e)
+        {
+            switch (operacion)
+            {
+                case "sumar":
+                    for (int i = 0; i < nFil; i++)
+                    {
+                        for (int j = 0; j < nCol; j++)
+                        {
+                            matriz1[i, j] += Convert.ToDecimal(dtgvDatos.Rows[i].Cells[j].Value);
+                        }
+                    }
+                    break;
+
+                case "restar":
+                    for (int i = 0; i < nFil; i++)
+                    {
+                        for (int j = 0; j < nCol; j++)
+                        {
+                            matriz1[i, j] = matriz1[i, j] - Convert.ToDecimal(dtgvDatos.Rows[i].Cells[j].Value);
+                        }
+                    }
+                    break;
+                case "prodem":
+                    AsignarMatriz();
+                    for (int i = 0; i < nFil; i++)
+                    {
+                        for (int j = 0; j < nCol; j++)
+                        {
+                            matriz1[i, j] = k * Convert.ToDecimal(dtgvDatos.Rows[i].Cells[j].Value);
+                        }
+                    }
+                    break;
+                case "producto":
+
+                    matriz2 = new decimal[nFil, nColB];
+
+                    for (int i = 0; i < nFil; i++)
+                    {
+                        for (int j = 0; j < nColB; j++)
+                        {
+                            for (int k = 0; k < nCol; k++)
+                            {
+                                matriz2[i, j] += matriz1[i, k] * Convert.ToDecimal(dtgvDatos.Rows[k].Cells[j].Value);
+                            }
+                        }
+                    }
+                    break;
+                case "traspuesta":
+                    for (int i = 0; i < nFil; i++)
+                    {
+                        for (int j = 0; j < nCol; j++)
+                        {
+                            dtgvResultado.Rows[j].Cells[i].Value = dtgvDatos.Rows[i].Cells[j].Value;
+                        }
+                    }
+                    break;
+            }
+            if (operacion != "traspuesta")
+            {
+                MostrarMatriz();
+            }
+        }
+
+        private void MostrarMatriz()
+        {
+            if (operacion == "producto")
+            {
+                for (int i = 0; i < nFil; i++)
+                {
+                    for (int j = 0; j < nColB; j++)
+                    {
+                        dtgvResultado.Rows[i].Cells[j].Value = matriz2[i, j].ToString();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < nFil; i++)
+                {
+                    for (int j = 0; j < nCol; j++)
+                    {
+                        dtgvResultado.Rows[i].Cells[j].Value = matriz1[i, j].ToString();
+                    }
+                }
+            }            
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            operacion = "sumar";
+            AsignarMatriz();
+            dtgvDatos.Columns.Clear();
+            DimensionMatrizA();
+            DimensionMatrizR();
+            txtFil.Enabled = false;
+            txtCol.Enabled = false;
+        }
+        
+
+        private void btnRestar_Click(object sender, EventArgs e)
+        {
+            operacion = "restar";
+            AsignarMatriz();
+            dtgvDatos.Columns.Clear();
+            DimensionMatrizA();
+            DimensionMatrizR();
+            txtFil.Enabled = false;
+            txtCol.Enabled = false;
+        }
+
+        private void btnPe_Click(object sender, EventArgs e)
+        {
+            operacion = "prodem";
+            k = Convert.ToInt32(txtFil.Text);
+            ClearControl();
+        }
+
+        private void btnPm_Click(object sender, EventArgs e)
+        {
+            operacion = "producto";
+            AsignarMatriz();
+            dtgvDatos.Columns.Clear();
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            CleanDtgd();
+            ClearControl();
+            nFil = 0;
+            nCol = 0;
+            nFilB = 0;
+            nColB = 0;
+            operacion = string.Empty;
+            txtvacio = false;
+            k = 0;
+            btnSumar.Enabled = false;
+            btnRestar.Enabled = false;
+            btnTras.Enabled = false;
+            btnPm.Enabled = false;
+            txtFil.Enabled = true;
+            txtCol.Enabled = true;
+            this.Refresh();
+        }
+
+        private void btnTras_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nFil; i++)
+            {
+                DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
+                column1.HeaderText = "N";
+                dtgvResultado.Columns.Add(column1);
+            }
+            dtgvResultado.Rows.Add(nCol);
+            operacion = "traspuesta";                        
+        }
+    }
+}
